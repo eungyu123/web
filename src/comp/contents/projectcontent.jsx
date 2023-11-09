8accbad03f460016e19a64694c81f76b7d592052
eungyu123 <150225773+eungyu123@users.noreply.github.com>
2023-11-09 10:12:36 +0900

    Delete src directory

--- src/comp/contents/projectcontent.jsx
+++
@@ -1,8 +0,0 @@
-const projectcontent =[
-    {gameimage:"/images/game1.png",
-    gametitle:"게임"},
-    {gameimage:"/images/game2.png",
-    gametitle:"게임"},
-]
-
-export default projectcontent;
--- src/comp/login/userdata.jsx
+++
@@ -1,5 +0,0 @@
-const userdata = { 
-        id:"ohbin",
-        pwd:"1017"}
-
-export default userdata;
